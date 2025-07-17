1585954a4479b372733583904a73eafe093b0100
Anjali Bhimani <109691875+anjalibhimani@users.noreply.github.com>
2025-07-17 11:35:19 +0200

    remove incorrect useRed import in About.js

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect} from 'react';
 import { User, Linkedin, Github, Download, Code, Coffee, Sparkles } from 'lucide-react';
 import TypeWriter from './TypeWriter.js';
 
